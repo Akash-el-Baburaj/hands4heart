065e0a14a6fbc648f4634915a3ac4bbf09d877b1
Elsa <elsamol@sanesquare.com>
2025-02-05 00:25:34 +0530

    video selection display

--- src/app/elements/widgets/recent-posts1/recent-posts1.component.ts
+++ src/app/elements/widgets/recent-posts1/recent-posts1.component.ts
@@ -1,3 +1,35 @@
+// import { Component, EventEmitter, Input, Output, OnInit, SimpleChanges } from '@angular/core';
+
+// @Component({
+//   selector: 'app-recent-posts1',
+//   templateUrl: './recent-posts1.component.html',
+//   styleUrls: ['./recent-posts1.component.css']
+// })
+// export class RecentPosts1Component implements OnInit {
+
+//     @Input()  data: any;
+//     @Output() courseSelected = new EventEmitter<any>(); // Emits selected course to parent
+//     courseDetails: any[] = [];
+  
+//     ngOnChanges(changes: SimpleChanges): void {
+//         if (changes['data']) {
+//           const Data = this.data
+//           this.courseDetails = Data?.details?.details
+//         }
+//       }
+
+//     ngOnInit(): void {
+//       // throw new Error('Method not implemented.');
+//     }
+
+//     getCourseVideo(videoUrl: string) {
+//       const data = videoUrl;
+//       this.courseSelected.emit(data)
+//     }
+
+    
+// }
+
 import { Component, EventEmitter, Input, Output, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
@@ -7,23 +39,25 @@ import { Component, EventEmitter, Input, Output, OnInit, SimpleChanges } from '@
 })
 export class RecentPosts1Component implements OnInit {
 
-    @Input()  data: any;
+    @Input() data: any;
     @Output() courseSelected = new EventEmitter<any>(); // Emits selected course to parent
     courseDetails: any[] = [];
-  
+    selectedCourse: any = null;  // Stores selected course
+
     ngOnChanges(changes: SimpleChanges): void {
         if (changes['data']) {
-          const Data = this.data
-          this.courseDetails = Data?.details?.details
+            this.courseDetails = this.data?.details?.details || [];
         }
-      }
-
-    ngOnInit(): void {
-      // throw new Error('Method not implemented.');
     }
 
-    getCourseVideo(videoUrl: string) {
-      const data = videoUrl;
-      this.courseSelected.emit(data)
+    ngOnInit(): void {}
+
+    getCourseVideo(course: any) {
+        this.selectedCourse = course;  // Set selected course
+        this.courseSelected.emit(course.video_url);
+    }
+
+    isSelected(course: any): boolean {
+        return this.selectedCourse === course;
     }
 }
